90f7ff8bc86a47100019ebc82fbd5ad632b4d085
gMemiy <am1369a@gmail.com>
2020-04-13 19:49:06 +0100

    some settings for remote

--- remote.sh
+++ remote.sh
@@ -11,21 +11,45 @@
 # plus decreased jankieness by switching search string from pressed to released
 #
 
+onF1()
+{
+ 	echo 'blue button pressed'
+	DISPLAY=:0 xdotool key f
+}
+onF2()
+{
+ 	echo 'red button pressed'
+	echo 'You are doing right'
+	pcmanfm /mnt/windows_share
+}
+onF3()
+{
+ 	echo 'green button pressed'
+	pcmanfm /mnt/windows_share
+}
+onF4()
+{
+ 	echo 'yellow button pressed'
+}
 onright() {
  	echo 'right button pressed'
-	DISPLAY=:0 xdotool key alt+Right
+	DISPLAY=:0 xdotool key Right
 }
 onleft() {
 	echo 'left button pressed'
-	DISPLAY=:0 xdotool key alt+Left
+	DISPLAY=:0 xdotool key Left
 }
 onnine() {
 	echo 'nine button pressed'
 	/home/pi/rpvlcdmp/./powerdown.sh
 }
+ondown() {
+	echo 'down button pressed'
+	DISPLAY=:0 xdotool key Down
+}
 onup() {
 	echo 'up button pressed'
-	DISPLAY=:0 xdotool key f
+	DISPLAY=:0 xdotool key Up
 }
 onselect() {
 	echo 'select button pressed'
@@ -51,6 +75,17 @@ ondisplay() {
 	echo 'display button pressed'
 	DISPLAY=:0 xdotool key i
 }
+onexit()
+{
+	echo 'exit button pressed'
+	DISPLAY=:0 xdotool key BackSpace
+
+}
+onclear()
+{
+	echo 'clear button pressed'
+	DISPLAY=:0 xdotool key alt+F4
+}
 
 filter() {
 	perl -nle 'BEGIN{$|=1} /key released: (.*) \(.*\)/ && print $1'
@@ -59,10 +94,17 @@ filter() {
 echo as | cec-client | filter | \
 while read cmd; do
 	case "$cmd" in
+		'F1 (blue)') onF1;;
+		'F2 (red)') onF2;;
+		'F3 (green)') onF3;;
+		'F4 (yellow)') onF4;;
+		clear) onclear;;
+		exit) onexit;;
 		right) onright;;
 		left) onleft;;
 		9) onnine;;
 		up) onup;;
+		down) ondown;;
 		select) onselect;;
 		play) onplay;;
 		pause) onpause;;
